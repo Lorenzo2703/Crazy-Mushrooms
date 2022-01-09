3d7d5f0e246a85217a682b6751698e8b4f8fc9e9
X shadow <gialollog@gmail.com>
2022-01-09 11:31:21 +0100

    countdown

--- js/scripts.js
+++ js/scripts.js
@@ -246,7 +246,7 @@ var x = setInterval(function () {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
 
