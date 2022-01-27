6c4e19dbef934ad8582707eef0c00c3ad6bb623b
X shadow <gialollog@gmail.com>
2022-01-27 09:57:36 +0100

    tag manager

--- js/scripts.js
+++ js/scripts.js
@@ -53,15 +53,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
   myFunction(); // Call listener function at run time
   document.addEventListener("change", myFunction()); // Attach listener function on state changes
 
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector("#mainNav");
-  if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: "#mainNav",
-      offset: 74,
-    });
-  }
-
   // Collapse responsive navbar when toggler is visible
   const navbarToggler = document.body.querySelector(".navbar-toggler");
   const responsiveNavItems = [].slice.call(
